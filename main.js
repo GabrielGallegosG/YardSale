eb5f3c3ed9ed0ccc1e531398d297a528a4354829
GabrielGallegosG <gallegosguzmang@gmail.com>
2023-01-26 09:00:07 -0800

    Se implementó el menú del carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const mobileHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 mobileHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+  desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive");
+function toggleMobileMenu() {
+  aside.classList.add("inactive");
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+  aside.classList.toggle("inactive");
 }
